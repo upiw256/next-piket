0684b0cc6605c5f83472e2cc5645c882f172f0ba
upiw256 <bilqimlb@gmail.com>
2023-11-10 04:37:53 +0700

    search and sweet alert

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,158 @@
-import Image from 'next/image'
+"use client";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import swal from "sweetalert";
 
-export default function Home() {
+function Page() {
+  const [data, setData] = useState([]);
+  const [nama, setNama] = useState("");
+  var no = 1;
+
+  const apiUrl = `http://103.229.14.238:3000/api/siswa?nama=${nama}`;
+
+  const handleNamaChange = (event) => {
+    setNama(event.target.value);
+  };
+
+  const fetchData = async () => {
+    if (nama) {
+      const headers = {
+        "x-barrier": "margaasih",
+      };
+
+      try {
+        const response = await axios.get(apiUrl, { headers });
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [nama]);
+  const btnIzin = (item) => {
+    swal({
+      title: item.nama,
+      text: "Apakah yakin untuk izin",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+      closeOnClickOutside: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        const dataAbsen = {
+          id_siswa: item.peserta_didik_id,
+          nisn: item.nisn,
+          nama: item.nama,
+          kelas: item.nama_rombel,
+          ket: "izin",
+        };
+        localStorage.setItem('absenData', JSON.stringify(dataAbsen));
+        const local = localStorage.getItem('absenData')
+        const jsonData = JSON.parse(local)
+        console.log(jsonData)
+        localStorage.removeItem('absenData');
+        swal(`data izin ${item.nama} tersimpan`, {
+          icon: "success",
+        });
+      }
+    });
+  };
+  const btnSakit = () => {
+    alert("Button Sakit");
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="h-screen w-full">
+      <div className="flex justify-center items-center">
+        <input
+          type="text"
+          value={nama}
+          onChange={handleNamaChange}
+          placeholder="Masukkan nama"
+          className="mt-3 input input-bordered input-neutral w-full max-w-xs"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <h1 className="ml-3">Data dari API:</h1>
+      {nama ? (
+        <div className="overflow-x-auto ">
+          <div className="hidden md:block">
+            <table className="table table-xs">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>NISN</th>
+                  <th>Nama</th>
+                  <th>Jenis Kelamin</th>
+                  <th>Tanggal Lahir</th>
+                  <th>Agama</th>
+                  <th>Alamat</th>
+                  <th>Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((item, index) => (
+                  <tr key={index}>
+                    <td>{no++}</td>
+                    <td>{item.nisn}</td>
+                    <td>{item.nama}</td>
+                    <td>{item.jenis_kelamin}</td>
+                    <td>{item.tanggal_lahir}</td>
+                    <td>{item.agama_id_str}</td>
+                    <td>{item.alamat_jalan}</td>
+                    <td>
+                      <button className="btn btn-primary mb-3 w-24">
+                        Izin
+                      </button>
+                      <button className="btn btn-warning mb-3 w-24">
+                        Sakit
+                      </button>
+                      <button className="btn btn-error mb-3 w-24">Alfa</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          {data.map((item) => (
+            <div className="card w-96 bg-info shadow-xl m-3">
+              <div className="card-body">
+                <h2 className="card-title">{item.nama}</h2>
+                ___________________________________________
+                <p>NISN: {item.nisn}</p>
+                <p>Jenis Kelamin: {item.jenis_kelamin}</p>
+                <p>Tanggal Lahir: {item.tanggal_lahir}</p>
+                <p>Kelas: {item.nama_rombel}</p>
+                <div className="card-actions justify-end">
+                  <button
+                    className="btn btn-primary"
+                    name="izin"
+                    onClick={() => btnIzin(item)}
+                  >
+                    Izin
+                  </button>
+                  <button
+                    className="btn btn-warning"
+                    name="sakit"
+                    onClick={btnSakit}
+                  >
+                    Sakit
+                  </button>
+                  <button className="btn btn-error" name="alfa">
+                    Alfa
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Inputkan data..</p>
+      )}
+    </div>
+  );
 }
+
+export default Page;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#4586f7",
+
+          "secondary": "#45ccd3",
+
+          "accent": "#a51349",
+
+          "neutral": "#1a191f",
+
+          "base-100": "#eef1f6",
+
+          "info": "#96bee4",
+
+          "success": "#32e29e",
+
+          "warning": "#f9da5d",
+
+          "error": "#ea7f7b",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
